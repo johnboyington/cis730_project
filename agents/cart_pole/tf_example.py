9e8fbfaa1ab02631df955e9f7df8dcfa79bdd8ad
johnboyington <mjcb@ksu.edu>
2018-12-07 10:55:44 -0600

    added another example

--- agents/cart_pole/tf_example.py
+++ agents/cart_pole/tf_example.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 mnist = tf.keras.datasets.mnist
+
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(),
@@ -16,3 +19,4 @@ model.compile(optimizer='adam',
 
 model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test, y_test)
+
