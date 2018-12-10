82b3b78293544771774dd6ee92948f9299c1235f
johnboyington <mjcb@ksu.edu>
2018-12-09 21:07:07 -0600

    now logs data with two feature screens as obs

--- agents/random_behavior/data.py
+++ agents/random_behavior/data.py
@@ -10,10 +10,12 @@ class Data_Container(object):
         """This method takes in a pysc2 timestep and an action index 
         and stores info from it."""
         
-        #obs = timestep.observation.feature_units
-        obs = [-1, -2, -3]
+        row = []
+        fs = timestep.observation.feature_screen
         
-        row = obs
+        for screen in [fs.unit_hit_points, fs.unit_type]:
+            row += list(screen.flatten())
+        
         row.append(timestep.reward)
         row.append(action)
         self.data.append(np.array(row))
@@ -23,7 +25,8 @@ class Data_Container(object):
     def save_data(self, filename):
         """A utility to store the data in a human readable numpy array."""
         self.data = np.array(self.data)
-        np.savetxt(filename, self.data, delimiter=',')
+        np.savetxt(filename, self.data, delimiter=',', fmt='%d')
+        return
                 
         
         
